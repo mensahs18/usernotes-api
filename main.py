df8ad48cd59edd36af573c8174748f365498891c
smensa <mensahl18@outlook.com>
2026-05-06 23:33:06 +0100

    Add /register endpoint with Argon2 hashing & validation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, LocalSession, Base
 from models import User
-from schemas import UserCreate
+from schemas import UserCreate, UserInDB
+from argon2 import PasswordHasher
 
+pwHasher = PasswordHasher()
 Base.metadata.create_all(engine)
 
 app = FastAPI()
@@ -20,20 +22,25 @@ def get_database():
 def read_root():
     return { "message": "Hello, Users!\n", "users" : "There are users here. Access /users to view them." }
 
-@app.get("/register")
+@app.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_database)):
-    
+    hashed_password = pwHasher.hash(user.password)
+
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if (existing_user != None):
+        raise HTTPException(401, detail="Username is already taken.")
+
     new_user = User(
         username=user.username,
-        password=user.password,
-        fname=user.fname,
-        sname=user.sname,
+        password=hashed_password,
+        fname=user.name.fname,
+        sname=user.name.sname,
     )
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return { "message": f"Registration successful.\nWelcome, {new_user.fname}.", "user_id:" : f"{new_user.id}",  }
+    return { "message": f"Registration successful.\nWelcome, {new_user.fname}.", "user_id:" : new_user.id,  }
 
 
--- schemas.py
+++ schemas.py
@@ -1,7 +1,41 @@
-from pydantic import BaseModel
+from typing import Annotated
+from pydantic import BaseModel, Field, StringConstraints, field_validator
+from argon2 import PasswordHasher
+import re
+
+strippedStr = Annotated[str, StringConstraints(strip_whitespace=True)]
+pwHasher = PasswordHasher()
+
+class Name(BaseModel):
+    fname: strippedStr
+    sname: strippedStr
 
 class UserCreate(BaseModel):
+    username: strippedStr = Field(min_length=3, frozen=True)
+    password: str = Field(min_length=8, max_lenth=128)
+    name: Name
+
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value):
+        if not re.search(r'[0-9]', value):
+            raise ValueError("Password must contain at least 1 number.")
+        if not re.search(r'[A-Z]', value):
+            raise ValueError("Password must contain at least uppercase letter.")
+        if not re.search(r'[a-z]', value):
+            raise ValueError("Password must contain at least lowercase letter.")
+        if not re.search(r'[ !"#$%&\'()*+,-./:;<=>?@\[\]^_`{|}~]', value):
+            raise ValueError("Password must contain at least one special character.")
+        if re.search(" ", value):
+            raise ValueError("Password must not contains spaces.")
+        
+        return value
+
+    
+
+class UserInDB(BaseModel):
     username: str
-    password: str
-    fname: str
-    sname: str
+    hashed_password: str
+    name: Name
+
+
