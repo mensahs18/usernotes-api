bab154b6ce27f7b6ad407d2807feedfa9b3ba744
smensa <mensahl18@outlook.com>
2026-06-07 23:02:43 +0100

    feat(db): implement notes table and Note model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from database import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
@@ -11,5 +12,24 @@ class User(Base):
     password: Mapped[str] = mapped_column()
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, username={self.username!r}, firstname={self.fname!r}, surname={self.sname!r}, password={self.password!r})"
+        return (
+            f"User(id={self.id!r}, " 
+            f"username={self.username!r}, "
+            f"firstname={self.fname!r}, "
+            f"surname={self.sname!r}"
+        )
     
+class Note(Base):
+    __tablename__ = "notes"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    title: Mapped[str] = mapped_column()
+    content: Mapped[str] = mapped_column()
+
+def __repr__(self) -> str:
+    return (
+        f"Note(id={self.id!r}, "
+        f"user_id={self.user_id!r}, "
+        f"title={self.title!r})"
+    )
