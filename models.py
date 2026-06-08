26a4981c56bca3616fcc67b5cf08ec2393dedecc
smensa <mensahl18@outlook.com>
2026-06-09 00:19:04 +0100

    feat(db): add created_at & updated_at columns in notes table

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from database import Base
 from uuid6 import UUID, uuid7
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, func, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -27,10 +28,15 @@ class Note(Base):
     user_id: Mapped[str] = mapped_column(String(36), ForeignKey("users.id"))
     title: Mapped[str] = mapped_column()
     content: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
 
     def __repr__(self) -> str:
         return (
             f"Note(id={self.id!r}, "
             f"user_id={self.user_id!r}, "
-            f"title={self.title!r})"
+            f"title={self.title!r}, "
+            f"content={self.content!r}, "
+            f"created_at={self.created_at!r}, "
+            f"updated_at={self.updated_at!r})"
         )
