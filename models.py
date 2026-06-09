2de917dadcac76c88c5680de9a880a1a2d7282fe
smensa <mensahl18@outlook.com>
2026-06-09 13:58:28 +0100

    feat(schema): add note create and response schema

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[UUID] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid7()))
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid7()))
     username: Mapped[str] = mapped_column(unique=True)
     fname: Mapped[str] = mapped_column()
     sname: Mapped[str] = mapped_column()
--- schemas.py
+++ schemas.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from pydantic import BaseModel, Field, StringConstraints, field_validator
 from argon2 import PasswordHasher
 from uuid6 import UUID
+from datetime import datetime
 import re
 
 strippedStr = Annotated[str, StringConstraints(strip_whitespace=True)]
@@ -22,9 +23,9 @@ class UserCreate(BaseModel):
         if not re.search(r'[0-9]', value):
             raise ValueError("Password must contain at least 1 number.")
         if not re.search(r'[A-Z]', value):
-            raise ValueError("Password must contain at least uppercase letter.")
+            raise ValueError("Password must contain at least one uppercase letter.")
         if not re.search(r'[a-z]', value):
-            raise ValueError("Password must contain at least lowercase letter.")
+            raise ValueError("Password must contain at least one lowercase letter.")
         if not re.search(r'[ !"#$%&\'()*+,-./:;<=>?@\[\]^_`{|}~]', value):
             raise ValueError("Password must contain at least one special character.")
         if re.search(" ", value):
@@ -46,3 +47,15 @@ class TokenPayload(BaseModel):
     sub: str
     exp: int
     iat: int
+
+
+class NoteCreate(BaseModel):
+    title: strippedStr = Field(min_length=1)
+    content: strippedStr = Field(min_length=1)
+
+class NoteResponse(BaseModel):
+    note_id: str
+    title: str
+    content: str
+    created_at: datetime
+    updated_at: datetime
