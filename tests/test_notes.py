1f20d124b32e44af7869aa3b4a060c2fcbdad4b9
smensa <mensahl18@outlook.com>
2026-07-05 21:58:09 +0100

    test(routes): add tests for creating notes, POST /notes

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -26,3 +26,48 @@ def authed_client(client):
     client.headers["Authorization"] = f"Bearer {token}"
 
     return client
+
+def test_create_note_successful(authed_client):
+    title = "Test Note A"
+    content = "This is the body of A"
+    response = authed_client.post(
+        "/notes",
+        json={
+            "title": title,
+            "content": content
+        })
+
+
+    assert response.status_code == 201
+    assert response.json()["title"] == title
+    assert response.json()["content"] == content
+
+@pytest.mark.parametrize("title, content", [
+    ("", "titleless content"),
+    ("title with no content", ""),
+])
+def test_create_note_missing_fields(authed_client, title, content):
+
+    response = authed_client.post(
+        "/notes",
+        json={
+            "title": title,
+            "content": content
+        })
+
+
+    assert response.status_code == 422
+
+def test_create_note_unauthenticated(client):
+
+    response = client.post(
+        "/notes",
+        json={
+            "title": "Test Note B",
+            "content": "This is B's body"
+        }
+    )
+
+    assert response.status_code == 401
+    
+    
