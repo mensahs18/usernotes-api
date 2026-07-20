eff3a6348181b08721e1e3aa75ddbf86dec72bef
smensa <mensahl18@outlook.com>
2026-07-20 14:14:26 +0100

    refactor(app): add async db init via lifespan

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from database import engine, Base
 from routes import user_router, note_router
 
-Base.metadata.create_all(engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/", status_code=200, tags=["root"])
 def read_root():
