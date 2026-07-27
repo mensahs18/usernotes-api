ad98e8d680dc411c3bdb72481f0d63e448bb1f63
smensa <mensahl18@outlook.com>
2026-07-27 15:51:52 +0100

    refactor(tests): convert notes tests to support async

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,13 +1,13 @@
 import pytest
 
 @pytest.fixture
-def authed_client(client):
-    create_and_login_user(client, "username0")
+async def authed_client(client):
+    await create_and_login_user(client, "username0")
 
     return client
 
-def create_and_login_user(client, username, password="ValidPassword1!"):
-    client.post(
+async def create_and_login_user(client, username, password="ValidPassword1!"):
+    await client.post(
         "/users/register",
         json={
             "username": username,
@@ -19,7 +19,7 @@ def create_and_login_user(client, username, password="ValidPassword1!"):
         }
     )
 
-    response = client.post(
+    response = await client.post(
         "/users/login",
         data={
             "username": username,
@@ -32,13 +32,14 @@ def create_and_login_user(client, username, password="ValidPassword1!"):
 
     return client
 
-def test_root(client):
-    assert client.get("/").status_code == 200
+async def test_root(client):
+    response = await client.get("/")
+    assert response.status_code == 200
 
-def test_create_note_successful(authed_client):
+async def test_create_note_successful(authed_client):
     title = "Test Note A"
     content = "This is the body of A"
-    response = authed_client.post(
+    response = await authed_client.post(
         "/notes",
         json={
             "title": title,
@@ -54,9 +55,9 @@ def test_create_note_successful(authed_client):
     ("", "titleless content"),
     ("title with no content", ""),
 ])
-def test_create_note_missing_fields(authed_client, title, content):
+async def test_create_note_missing_fields(authed_client, title, content):
 
-    response = authed_client.post(
+    response = await authed_client.post(
         "/notes",
         json={
             "title": title,
@@ -66,9 +67,9 @@ def test_create_note_missing_fields(authed_client, title, content):
 
     assert response.status_code == 422
 
-def test_create_note_unauthenticated(client):
+async def test_create_note_unauthenticated(client):
 
-    response = client.post(
+    response = await client.post(
         "/notes",
         json={
             "title": "Test Note B",
@@ -79,12 +80,12 @@ def test_create_note_unauthenticated(client):
     assert response.status_code == 401
     
 
-def test_get_individual_note(authed_client):
+async def test_get_individual_note(authed_client):
 
     title = "Test Note C"
     content = "Body of C"
 
-    response = authed_client.post(
+    response = await authed_client.post(
     "/notes",
     json={
         "title": title,
@@ -95,7 +96,7 @@ def test_get_individual_note(authed_client):
 
     note_id = response.json()["id"]
 
-    get_response = authed_client.get(f"/notes/{note_id}")
+    get_response = await authed_client.get(f"/notes/{note_id}")
 
     assert get_response.status_code == 200, "/GET note failed"
 
@@ -104,14 +105,14 @@ def test_get_individual_note(authed_client):
     assert get_response.json()["content"] == content
 
 
-def test_get_invalid_note(authed_client):
-    response = authed_client.get("/notes/019484b2-f300-7000-8000-123456789abc")
+async def test_get_invalid_note(authed_client):
+    response = await authed_client.get("/notes/019484b2-f300-7000-8000-123456789abc")
 
     assert response.status_code == 404
 
-def test_get_note_unauthorized_user(authed_client):
+async def test_get_note_unauthorized_user(authed_client):
 
-    note_response = authed_client.post(
+    note_response = await authed_client.post(
     "/notes",
     json={
         "title": "Foo Note",
@@ -122,14 +123,14 @@ def test_get_note_unauthorized_user(authed_client):
 
     note_id = note_response.json()["id"]
 
-    create_and_login_user(authed_client, "username1")
+    await create_and_login_user(authed_client, "username1")
 
-    response = authed_client.get(f"/notes/{note_id}")
+    response = await authed_client.get(f"/notes/{note_id}")
 
     assert response.status_code == 404
 
-def test_get_notes(authed_client):
-    note_response_a = authed_client.post(
+async def test_get_notes(authed_client):
+    note_response_a = await authed_client.post(
         "/notes",
         json={
             "title": "Foo Note A", "content": "A's contents"
@@ -138,7 +139,7 @@ def test_get_notes(authed_client):
 
     assert note_response_a.status_code == 201, "Note initialisation fail"
 
-    note_response_b = authed_client.post(
+    note_response_b = await authed_client.post(
         "/notes",
         json={
             "title": "Foo Note B", "content": "B's contents"
@@ -147,7 +148,7 @@ def test_get_notes(authed_client):
 
     assert note_response_b.status_code == 201, "Note initialisation fail"
 
-    response = authed_client.get("/notes")
+    response = await authed_client.get("/notes")
 
     assert len(response.json()) == 2
 
@@ -155,19 +156,19 @@ def test_get_notes(authed_client):
     assert "Foo Note A" in titles
     assert "Foo Note B" in titles
 
-def test_get_notes_unauthenticated(client):
-    response = client.get("/notes")
+async def test_get_notes_unauthenticated(client):
+    response = await client.get("/notes")
     assert response.status_code == 401
 
-def test_get_notes_unauthorized_user(authed_client):
-    authed_client.post("/notes", json={
+async def test_get_notes_unauthorized_user(authed_client):
+    await authed_client.post("/notes", json={
         "title": "User0's note",
         "content": "note0 info"
     })
 
-    create_and_login_user(authed_client, "username1")
+    await create_and_login_user(authed_client, "username1")
     
-    authed_client.post(
+    await authed_client.post(
         "/notes", 
         json={
             "title": "User1's note", 
@@ -175,15 +176,15 @@ def test_get_notes_unauthorized_user(authed_client):
         }
     )
 
-    response = authed_client.get("/notes")
+    response = await authed_client.get("/notes")
     titles = [note["title"] for note in response.json()]
 
     assert len(response.json()) == 1
     assert "User1's note" in titles
     assert "User0's note" not in titles
 
-def test_update_note(authed_client):
-    create_response = authed_client.post(
+async def test_update_note(authed_client):
+    create_response = await authed_client.post(
         "/notes",
         json={
             "title": "Incorrect Title",
@@ -195,7 +196,7 @@ def test_update_note(authed_client):
 
     note_id = create_response.json()["id"]
 
-    response = authed_client.patch(
+    response = await authed_client.patch(
         f"/notes/{note_id}",
         json={
             "title": "Correct Title"
@@ -206,15 +207,15 @@ def test_update_note(authed_client):
     assert response.json()["title"] == "Correct Title"
     assert response.json()["content"] == "Correct Content"
 
-    get_response = authed_client.get(f"/notes/{note_id}")
+    get_response = await authed_client.get(f"/notes/{note_id}")
 
     assert get_response.status_code == 200, "Note not retrieved by /GET"
     assert get_response.json()["title"] == "Correct Title"
     assert get_response.json()["content"] == "Correct Content"
 
-def test_update_note_unauthenticated(client):
+async def test_update_note_unauthenticated(client):
 
-    response = client.patch("/notes/019484b2-f300-7000-8000-123456789abc",
+    response = await client.patch("/notes/019484b2-f300-7000-8000-123456789abc",
         json={
             "title": "Patched Title",
             "content": "Patched Content"
@@ -223,9 +224,9 @@ def test_update_note_unauthenticated(client):
     
     assert response.status_code == 401
 
-def test_update_note_unauthorized_user(authed_client):
+async def test_update_note_unauthorized_user(authed_client):
 
-    note_response = authed_client.post(
+    note_response = await authed_client.post(
         "/notes",
         json={
             "title": "Foo Note",
@@ -237,9 +238,9 @@ def test_update_note_unauthorized_user(authed_client):
 
     note_id = note_response.json()["id"]
 
-    create_and_login_user(authed_client, "username1")
+    await create_and_login_user(authed_client, "username1")
 
-    response = authed_client.patch(
+    response = await authed_client.patch(
         f"/notes/{note_id}",
         json={
             "title": "Hacked Title"
@@ -248,8 +249,8 @@ def test_update_note_unauthorized_user(authed_client):
 
     assert response.status_code == 404
 
-def test_update_invalid_note(authed_client):
-    response = authed_client.patch("/notes/019484b2-f300-7000-8000-123456789abc",
+async def test_update_invalid_note(authed_client):
+    response = await authed_client.patch("/notes/019484b2-f300-7000-8000-123456789abc",
         json={
             "title": "Invalid title",
             "content": "No base content"
@@ -258,8 +259,8 @@ def test_update_invalid_note(authed_client):
 
     assert response.status_code == 404
 
-def test_delete_note(authed_client):
-    create_response = authed_client.post(
+async def test_delete_note(authed_client):
+    create_response = await authed_client.post(
         "/notes",
         json={
             "title": "Test note",
@@ -271,17 +272,17 @@ def test_delete_note(authed_client):
 
     note_id = create_response.json()["id"]
 
-    response = authed_client.delete(f"/notes/{note_id}")
+    response = await authed_client.delete(f"/notes/{note_id}")
 
     assert response.status_code == 204
 
-def test_delete_note_unauthenticated(client):
-    response = client.delete(f"/notes/019484b2-f300-7000-8000-123456789abc")
+async def test_delete_note_unauthenticated(client):
+    response = await client.delete(f"/notes/019484b2-f300-7000-8000-123456789abc")
 
     assert response.status_code == 401
 
-def test_delete_note_unauthorized_user(authed_client):
-    note_response = authed_client.post(
+async def test_delete_note_unauthorized_user(authed_client):
+    note_response = await authed_client.post(
         "/notes",
         json={
             "title": "By User0",
@@ -293,8 +294,8 @@ def test_delete_note_unauthorized_user(authed_client):
 
     note_id = note_response.json()["id"]
 
-    create_and_login_user(authed_client, "username1")
+    await create_and_login_user(authed_client, "username1")
 
-    response = authed_client.delete(f"/notes/{note_id}")
+    response = await authed_client.delete(f"/notes/{note_id}")
 
     assert response.status_code == 404
