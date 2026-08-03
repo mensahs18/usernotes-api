a73405f98ec2159288c4709fd365d0f6634c3d3b
smensa <mensahl18@outlook.com>
2026-08-03 13:49:50 +0100

    feat(auth): implement AESGCM encrypt() and decrypt() functions

---
+++ auth/encryption.py
@@ -0,0 +1,25 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+hex_key = os.getenv("ENCRYPT_KEY")
+ENC_KEY = bytes.fromhex(hex_key)
+
+def encrypt(text: str) -> str:
+    
+    aesgcm = AESGCM(ENC_KEY)
+    nonce = os.urandom(12)
+    cipher = aesgcm.encrypt(nonce=nonce, data=text.encode(), associated_data=None)
+
+    return (nonce + cipher).hex()
+
+def decrypt(encrypted_hex: str) -> str:
+    encrypted = bytes.fromhex(encrypted_hex)
+
+    nonce = encrypted[:12]
+    cipher = encrypted[12:]
+    aesgcm = AESGCM(ENC_KEY)
+
+    decrypted_bytes = aesgcm.decrypt(nonce=nonce, data=cipher, associated_data=None)
+    return decrypted_bytes.decode()
