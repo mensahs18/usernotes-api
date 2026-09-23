77bbd4d82ae592633b9948ea7b61a8c11046129e
smensa <mensahl18@outlook.com>
2026-09-23 17:41:43 +0100

    refactor(models): add db index to note's user_id foreign key and cascading deletion.
    Add cascade on_delete to accomodate future support for user removal.
    Update README design decisions and env setup. Override the env url in docker to allow correct url for both alembic and container.

--- app/models/note.py
+++ app/models/note.py
@@ -12,7 +12,9 @@ class Note(Base):
     __tablename__ = "notes"
 
     id: Mapped[UUID] = mapped_column(Uuid, primary_key=True, default=lambda: uuid7())
-    user_id: Mapped[UUID] = mapped_column(Uuid, ForeignKey("users.id"))
+    user_id: Mapped[UUID] = mapped_column(
+        Uuid, ForeignKey("users.id", ondelete="CASCADE"), index=True
+    )
     title: Mapped[str] = mapped_column()
     content: Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
